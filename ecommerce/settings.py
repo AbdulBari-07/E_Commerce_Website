e0f23726d9d6ada0fa5ffeff389e52065fd6bbed
Abdul Bari <67307693+AbdulBari-07@users.noreply.github.com>
2020-09-29 13:08:53 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -117,12 +117,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find staic files
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(PROJECT_ROOT, 'static'),
 ]
 
 MEDIA_URL = '/images/'
